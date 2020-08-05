969fc4ee423eae459e2ce13e7f260a33ceceeb52
Mahmoud Atef <mahmoudatif54@yahoo.com>
2020-08-05 21:03:32 +0200

    update

--- lib/login.dart
+++ lib/login.dart
@@ -3,8 +3,7 @@ import 'package:new_app/colors.dart';
 import 'package:new_app/services/login_with_google.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:new_app/widgets/widgets.dart';
-import 'create-account.dart';
-import 'home.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -21,6 +20,7 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
   bool _success;
   String _userEmail;
+  bool _showSpinner = false;
 
   ChooseType _type = ChooseType.isCompany;
   void _signInWithEmailAndPassword() async {
@@ -36,11 +36,23 @@ class _LoginPageState extends State<LoginPage> {
           _success = true;
           _userEmail = user.email;
         });
+        imageUrl =
+            'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.0-9/16196015_10154888128487744_6901111466535510271_n.png?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=fwASFdSqsjAAX9vvrkr&_nc_ht=scontent.fcai21-1.fna&oh=310408dea3df86cfd4e1c18a36f0987c&oe=5F4FC717';
+        name = _userEmail;
+        switch (_type) {
+          case ChooseType.isCompany:
+            Navigator.of(context).pushNamed('/first');
+            break;
+          case ChooseType.isStudent:
+            Navigator.of(context).pushNamed('/homePage');
+            break;
+        }
       } else {
-        setState(() {
-          _success = false;
-        });
+        print("Error");
       }
+      setState(() {
+        _showSpinner = false;
+      });
     } catch (e) {
       print(e);
     }
@@ -56,81 +68,74 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("images/bg.png"),
-              fit: BoxFit.cover,
+      body: ModalProgressHUD(
+        inAsyncCall: _showSpinner,
+        child: Form(
+          key: _formKey,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/bg.png"),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: SafeArea(
-            child: ListView(
-              padding: EdgeInsets.symmetric(horizontal: 24.0),
-              children: <Widget>[
-                SizedBox(
-                  height: 80.0,
-                ),
-                Column(
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: AssetImage("images/logo.png"),
-                      radius: 50,
-                      backgroundColor: Colors.transparent,
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-                buildFormFile('email', _emailController, false, Icons.email),
-                SizedBox(
-                  height: 20,
-                ),
-                buildFormFile(
-                    'password', _passwordController, true, Icons.lock),
-                buildListTile('Company', ChooseType.isCompany),
-                buildListTile('Student', ChooseType.isStudent),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5.0, 15.0, 30.0, 0.0),
-                  child: ButtonBar(
-                    mainAxisSize: MainAxisSize.min,
+            child: SafeArea(
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                children: <Widget>[
+                  SizedBox(
+                    height: 80.0,
+                  ),
+                  Column(
                     children: <Widget>[
-                      FlatButton(
-                        child: Text(
-                          "Create",
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).pushNamed('/createAcount');
-                        },
-                      ),
-                      RaisedButton(
-                        child: Text("Sign In"),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            _signInWithEmailAndPassword();
-                            if (_success) {
-                              imageUrl =
-                                  'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.0-9/16196015_10154888128487744_6901111466535510271_n.png?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=fwASFdSqsjAAX9vvrkr&_nc_ht=scontent.fcai21-1.fna&oh=310408dea3df86cfd4e1c18a36f0987c&oe=5F4FC717';
-                              name = _userEmail;
-                              switch (_type) {
-                                case ChooseType.isCompany:
-                                  Navigator.of(context).pushNamed('/first');
-                                  break;
-                                case ChooseType.isStudent:
-                                  Navigator.of(context).pushNamed('/homePage');
-                                  break;
-                              }
-                            }
-                          }
-                        },
-                      ),
+                      CircleAvatar(
+                        backgroundImage: AssetImage("images/logo.png"),
+                        radius: 50,
+                        backgroundColor: Colors.transparent,
+                      )
                     ],
                   ),
-                ),
-                buildButtonBar(context),
-              ],
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  buildFormFile('email', _emailController, false, Icons.email),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  buildFormFile(
+                      'password', _passwordController, true, Icons.lock),
+                  buildListTile('Company', ChooseType.isCompany),
+                  buildListTile('Student', ChooseType.isStudent),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(5.0, 15.0, 30.0, 0.0),
+                    child: ButtonBar(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        FlatButton(
+                          child: Text(
+                            "Create",
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/createAcount');
+                          },
+                        ),
+                        RaisedButton(
+                          child: Text("Sign In"),
+                          onPressed: () async {
+                            setState(() {
+                              _showSpinner = true;
+                            });
+                            if (_formKey.currentState.validate()) {
+                              _signInWithEmailAndPassword();
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  buildButtonBar(context),
+                ],
+              ),
             ),
           ),
         ),
